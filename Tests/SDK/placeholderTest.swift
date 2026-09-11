bb44ebf407e4e4413c0a2cc9dd31265cf8fb4eb8
Banks, Kyle <kyle.banks@breadfinancial.com>
2026-09-11 13:29:43 -0500

    Add placeholder unit tests for the SDK

---
+++ Tests/SDK/placeholderTest.swift
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+//  File:          placeholderTest.swift
+//  Author(s):     Bread Financial
+//  Date:          02 September 2026
+//
+//  Descriptions:  Placeholder unit tests for the SDK.
+//
+//  © 2026 Bread Financial
+//------------------------------------------------------------------------------
+
+import Testing
+@testable import BreadPartnersSDKCore
+
+@Suite struct PlaceholderSDKTest {
+    @Test
+    func basicInitialization() {
+        #expect(true == true)
+    }
+
+}
