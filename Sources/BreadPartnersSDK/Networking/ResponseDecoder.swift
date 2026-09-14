86a1f01c7af38cc27921e660985cf3daa64ab779
kylebanks-bread <kyle.banks@breadfinancial.com>
2026-09-14 11:11:08 -0500

    Bread partners rename (#59)
    Co-authored-by: Banks, Kyle <kyle.ryan.banks@gmail.com>
    Co-authored-by: Joncarlos Tavarez <joncarlos.tavarez@breadfinancial.com>

--- Sources/BreadPartnersSDK/Networking/ResponseDecoder.swift
+++
@@ -1,9 +0,0 @@
-import BreadPartnersSDKCore
-import Foundation
-
-internal enum ResponseDecoder {
-    static func decode<T: Decodable>(_ response: Any, as type: T.Type) throws -> T {
-        let payload = (response as? AnySendable)?.value ?? response
-        return try BreadPartnersSDKCore.decodeJSON(from: payload, to: type)
-    }
-}
