184c6240906eb1c990089e720e2c1f0fdca17f63
Banks, Kyle <kyle.banks@breadfinancial.com>
2026-09-14 18:22:13 -0500

    Add RTPSDependencies class and corresponding unit tests for dependency injection

--- Sources/BreadPartnersCore/Domains/RTPS/RTPSDependencies.swift
+++ Sources/BreadPartnersCore/Domains/RTPS/RTPSDependencies.swift
@@ -1,7 +1,18 @@
 package struct RTPSDependencies: Sendable {
     package let recaptcha: any RecaptchaProviding
+    package let network: any RTPSNetworkClient
+    package let requestBuilder: any RTPSRequestBuilding
+    package let responseDecoder: any RTPSResponseDecoding
 
-    package init(recaptcha: any RecaptchaProviding) {
+    package init(
+        recaptcha: any RecaptchaProviding,
+        network: any RTPSNetworkClient,
+        requestBuilder: any RTPSRequestBuilding,
+        responseDecoder: any RTPSResponseDecoding
+    ) {
         self.recaptcha = recaptcha
+        self.network = network
+        self.requestBuilder = requestBuilder
+        self.responseDecoder = responseDecoder
     }
 }
