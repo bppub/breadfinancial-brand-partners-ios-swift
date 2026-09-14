67f8a4b20a4f2e797bcb78f08f8f5ac7aeff9f2f
Banks, Kyle <kyle.banks@breadfinancial.com>
2026-09-14 18:21:41 -0500

    Add RTPSResult enum and corresponding unit tests for return code mapping

---
+++ Sources/BreadPartnersCore/Domains/RTPS/RTPSResult.swift
@@ -0,0 +1,28 @@
+package enum RTPSResult: Sendable, Equatable {
+    case accountFound
+    case approved
+    case noHit
+    case makeOffer
+    case acknowledge
+
+    package init(returnCode: String?) {
+        switch returnCode {
+        case "0":
+            self = .accountFound
+        case "01":
+            self = .approved
+        case "11":
+            self = .makeOffer
+        case "12":
+            self = .acknowledge
+        default:
+            self = .noHit
+        }
+    }
+}
+
+package typealias PrescreenResult = RTPSResult
+
+package func getPrescreenResult(from apiResponse: String) -> RTPSResult {
+    RTPSResult(returnCode: apiResponse)
+}
